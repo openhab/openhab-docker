ea08d0f4f9200489e743056d8404a4a284821b2d
Christian Häussler <c-n-i@web.de>
2017-01-29 23:22:52 +0100

    Fix Dockerfile permission issue
    Signed-off-by: Christian Häussler <c-n-i@web.de> (github: @cniweb)

--- 1.8.3/armhf/entrypoint.sh
+++ 1.8.3/armhf/entrypoint.sh
@@ -7,8 +7,6 @@ if [ "$1" = 'server' ] || [ "$1" = 'openhab' ]; then
   gosu openhab "${APPDIR}/start.sh"
 elif [ "$1" = 'debug' ]; then
   gosu openhab "${APPDIR}/start_debug.sh"
-elif [ "$1" = 'console' ] || [ "$1" = 'shell' ]; then
-  gosu openhab "${APPDIR}/runtime/bin/client"
 else
   gosu openhab "$@"
 fi
