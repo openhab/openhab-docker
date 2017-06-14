7d60e758a1f453f14639857d53cee65339b16333
legacycode <legacycode@users.noreply.github.com>
2017-06-14 12:47:16 +0200

    Hot fix for issue #90 (#93)
    * fixed broken build for arm64. migrated from ubuntu to debian jessie. migrated from oraclejdk to openjdk8
    
    * Removed FLAVOR from build files. Rework on readme.md
    
    * removed gosu integration check
    
    * removed gosu integration check

--- update.sh
+++ update.sh
@@ -131,8 +131,7 @@ print_gosu() {
 	    && gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
 	    && gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu \
 	    && rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc \
-	    && chmod +x /usr/local/bin/gosu \
-	    && gosu nobody true
+	    && chmod +x /usr/local/bin/gosu
 
 EOI
 }
