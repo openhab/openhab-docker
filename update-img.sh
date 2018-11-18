4ad63786eaf4a46cddc16092dc57a6718e019bc5
cniweb <c-n-i@web.de>
2018-11-19 00:57:54 +0100

    Add sudo for install img
    Signed-off-by: cniweb <c-n-i@web.de>

--- update-img.sh
+++ update-img.sh
@@ -4,7 +4,7 @@
 export IMG_SHA256="6b7b660fa0a4c4ab10aa2c2d7d586afdbc70cb33644995b0ee0e7f77ddcc2565"
 
 # Download and check the sha256sum.
-curl -fSL "https://github.com/genuinetools/img/releases/download/v0.5.4/img-linux-amd64" -o "/usr/local/bin/img" \
+sudo curl -fSL "https://github.com/genuinetools/img/releases/download/v0.5.4/img-linux-amd64" -o "/usr/local/bin/img" \
 	&& echo "${IMG_SHA256}  /usr/local/bin/img" | sha256sum -c - \
 	&& chmod a+x "/usr/local/bin/img"
 
