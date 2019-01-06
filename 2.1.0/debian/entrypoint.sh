65b330b93aba87d68caba8a88f4e298e058bc50b
Wouter Born <eclipse@maindrain.net>
2019-01-06 20:30:00 +0100

    Rename existing group matching GROUP_ID environment variable to openhab
    Signed-off-by: Wouter Born <eclipse@maindrain.net>

--- 2.1.0/debian/entrypoint.sh
+++ 2.1.0/debian/entrypoint.sh
@@ -27,8 +27,14 @@ NEW_USER_ID=${USER_ID:-9001}
 NEW_GROUP_ID=${GROUP_ID:-$NEW_USER_ID}
 echo "Starting with openhab user id: $NEW_USER_ID and group id: $NEW_GROUP_ID"
 if ! id -u openhab >/dev/null 2>&1; then
-  echo "Create group openhab with id ${NEW_GROUP_ID}"
-  groupadd -g $NEW_GROUP_ID openhab
+  if [ -z "$(getent group $NEW_GROUP_ID)" ]; then
+    echo "Create group openhab with id ${NEW_GROUP_ID}"
+    groupadd -g $NEW_GROUP_ID openhab
+  else
+    group_name=$(getent group $NEW_GROUP_ID | cut -d: -f1)
+    echo "Rename group $group_name to openhab"
+    groupmod --new-name openhab $group_name
+  fi
   echo "Create user openhab with id ${NEW_USER_ID}"
   adduser -u $NEW_USER_ID --disabled-password --gecos '' --home "${APPDIR}" --gid $NEW_GROUP_ID openhab
   groupadd -g 14 uucp2
